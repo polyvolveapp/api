5d43ea8346430b3ec64593c690c786d6086661ee
Diablo <info@m.demoneum.com>
2020-05-31 13:10:30 +0200

    Add cors origins of gitlab address.

--- src/main/kotlin/polyvolve/prototype/api/config/JWTConstants.kt
+++ src/main/kotlin/polyvolve/prototype/api/config/JWTConstants.kt
@@ -5,5 +5,5 @@ object JWTConstants {
     const val EXPIRATION_TIME = 864000000L // 10 days
     const val TOKEN_PREFIX = "Bearer "
     const val HEADER_STRING = "Authorization"
-    val allowedOrigins = listOf("http://localhost:3000", "http://localhost:3333")
+    val allowedOrigins = listOf("http://localhost:3000", "http://localhost:3333", "https://polyvolve-api.herokuapp.com")
 }
